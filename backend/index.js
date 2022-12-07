bfb7931adc27f649909929fde1a4e39c8001bf11
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-07 16:50:50 -0500

    Updated .env file

--- backend/index.js
+++ backend/index.js
@@ -32,3 +32,9 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/* MONGOOSE */
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+}).catch((err) => console.log(`${err} Did not connect`));
