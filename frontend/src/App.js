11db7703cf565e38c525287cfff862a16de6443c
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-07 18:39:15 -0500

    Finished skeleton framework of app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import { Cssbaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { themeSettings } from 'theme';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-
+      <ThemeProvider theme={theme}>
+        <Cssbaseline />
+        <h1>hello world</h1>
+      </ThemeProvider>
     </div>
   );
 }
