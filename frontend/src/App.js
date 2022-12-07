e2d8bfeb2a671a2b431756a226b0bf7bc5e4c89b
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-07 18:37:22 -0500

    Added picture, state, redux and mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-
+import { Cssbaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
   return (
     <div className="app">
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import './index.css';
+import globalReducer from 'state';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
