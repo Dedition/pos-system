83b57017ad87683bcc42c522f9bb882af0b1e8a9
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-09 00:59:47 -0500

    Finished fixing app and building skeleton of Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { themeSettings } from 'theme';
+
+import Navbar from 'components/NavbarPage/Navbar';
+import Dashboard from 'components/DashboardPage/Dashboard';
+import Layout from 'components/LayoutPage/Layout';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <h1>hello world</h1>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />} />
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
