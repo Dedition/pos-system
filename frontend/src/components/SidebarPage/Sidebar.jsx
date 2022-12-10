729207591f72d67cdb81f5da1eb76febdc24c959
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-10 01:18:11 -0500

    Imported all relevant imports

--- frontend/src/components/SidebarPage/Sidebar.jsx
+++ frontend/src/components/SidebarPage/Sidebar.jsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "../Styles/FlexBetween";
+import profilepic from "../../assets/profilepic.jpeg";
+import {
+    Box, Drawer, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography, useMediaQuery, useTheme,
+} from "@mui/material";
+import {
+    SettingsOutlined,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMonthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined,
+} from "@mui/icons-material";
 
+const Sidebar = ({
+    isNonMobile, drawerWidth, isSideBarOpen, setIsSideBarOpen,
+}) => {
+    const { pathname } = useLocation();
+    const [active, setActive] = useState("");
+    const navigate = useNavigate();
+    const theme = useTheme();
 
-const Sidebar = () => {
+    useEffect(() => { setActive(pathname.split("/")[1]); }, [pathname]);
+
     return (
-        <div>Sidebar</div>
+        <Box component="nav">
+
+        </Box>
     )
 }
 
