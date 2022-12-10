babb387e7c6c4fa84725dc841568d9195847c7d3
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-10 16:23:33 -0500

    Extracteed Navitems to its own file

--- frontend/src/components/SidebarPage/Sidebar.jsx
+++ frontend/src/components/SidebarPage/Sidebar.jsx
@@ -6,81 +6,12 @@ import {
     Box, Drawer, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography, useMediaQuery, useTheme, ListItemButton,
 } from "@mui/material";
 import {
-    SettingsOutlined,
     ChevronLeft,
     ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    PointOfSaleOutlined,
-    TodayOutlined,
-    CalendarMonthOutlined,
-    AdminPanelSettingsOutlined,
-    TrendingUpOutlined,
-    PieChartOutlined,
 } from "@mui/icons-material";
+import { navItems } from "./NavItems";
 
 
-const navItems = [
-    {
-        text: "Dashboard",
-        icon: <HomeOutlined />,
-    },
-    {
-        text: "Client Facing",
-        icon: null,
-    },
-    {
-        text: "Products",
-        icon: <ShoppingCartOutlined />,
-    },
-    {
-        text: "Customers",
-        icon: <Groups2Outlined />,
-    },
-    {
-        text: "Transactions",
-        icon: <ReceiptLongOutlined />,
-    },
-    {
-        text: "Geeography",
-        icon: <PublicOutlined />,
-    },
-    {
-        text: "Sales",
-        icon: null
-    },
-    {
-        text: "Overview",
-        icon: <PointOfSaleOutlined />,
-    },
-    {
-        text: "Daily",
-        icon: <TodayOutlined />,
-    },
-    {
-        text: "Monthly",
-        icon: <CalendarMonthOutlined />,
-    },
-    {
-        text: "Breakdown",
-        icon: <PieChartOutlined />,
-    },
-    {
-        text: "Management",
-        icon: null,
-    },
-    {
-        text: "Admin",
-        icon: <AdminPanelSettingsOutlined />,
-    },
-    {
-        text: "Performance",
-        icon: <TrendingUpOutlined />,
-    },
-]
 const Sidebar = ({
     isNonMobile, drawerWidth, isSideBarOpen, setIsSideBarOpen,
 }) => {
