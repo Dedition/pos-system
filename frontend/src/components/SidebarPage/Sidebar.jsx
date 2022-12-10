5cf89ea4c8f60fcd7e752d7388c15b9ba7b22b30
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-10 02:16:23 -0500

    Got sidebar to open.  Need to fix margin

--- frontend/src/components/SidebarPage/Sidebar.jsx
+++ frontend/src/components/SidebarPage/Sidebar.jsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "../Styles/FlexBetween";
 import profilepic from "../../assets/profilepic.jpeg";
 import {
-    Box, Drawer, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography, useMediaQuery, useTheme,
+    Box, Drawer, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography, useMediaQuery, useTheme, ListItemButton,
 } from "@mui/material";
 import {
     SettingsOutlined,
@@ -22,6 +22,65 @@ import {
     PieChartOutlined,
 } from "@mui/icons-material";
 
+
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <HomeOutlined />,
+    },
+    {
+        text: "Client Facing",
+        icon: null,
+    },
+    {
+        text: "Products",
+        icon: <ShoppingCartOutlined />,
+    },
+    {
+        text: "Customers",
+        icon: <Groups2Outlined />,
+    },
+    {
+        text: "Transactions",
+        icon: <ReceiptLongOutlined />,
+    },
+    {
+        text: "Geeography",
+        icon: <PublicOutlined />,
+    },
+    {
+        text: "Sales",
+        icon: null
+    },
+    {
+        text: "Overview",
+        icon: <PointOfSaleOutlined />,
+    },
+    {
+        text: "Daily",
+        icon: <TodayOutlined />,
+    },
+    {
+        text: "Monthly",
+        icon: <CalendarMonthOutlined />,
+    },
+    {
+        text: "Breakdown",
+        icon: <PieChartOutlined />,
+    },
+    {
+        text: "Management",
+        icon: null,
+    },
+    {
+        text: "Admin",
+        icon: <AdminPanelSettingsOutlined />,
+    },
+    {
+        text: "Performance",
+        icon: <TrendingUpOutlined />,
+    },
+]
 const Sidebar = ({
     isNonMobile, drawerWidth, isSideBarOpen, setIsSideBarOpen,
 }) => {
@@ -34,28 +93,94 @@ const Sidebar = ({
 
     return (
         <Box component="nav">
-            [isSideBarOpen && (
-            <Drawer
-                open={isSideBarOpen}
-                onClose={() => setIsSideBarOpen(false)}
-                variant={isNonMobile ? "persistent" : "temporary"}
-                anchor="left"
-                sx={{
-                    width: drawerWidth,
-                    "& .MuiDrawer-paper": {
-                        color: theme.palette.secondary[200],
-                        backgroundColor: theme.palette.background.alt,
-                        boxSizing: "border-box",
-                        borderWidth: isNonMobile ? 0 : "2px",
-                        width: drawerWidth
-                    }
-                }}
-            >
+            {isSideBarOpen && (
+                <Drawer
+                    open={isSideBarOpen}
+                    onClose={() => setIsSideBarOpen(false)}
+                    variant="persistent"
+                    anchor="left"
+                    sx={{
+                        width: drawerWidth,
+                        "& .MuiDrawer-paper": {
+                            color: theme.palette.secondary[200],
+                            backgroundColor: theme.palette.background.alt,
+                            boxSixing: "border-box",
+                            borderWidth: isNonMobile ? 0 : "2px",
+                            width: drawerWidth,
+                        },
+                    }}
+                >
+                    <Box width="100%">
+                        <Box m="1.5rem 2rem 2rem 3rem">
+                            <FlexBetween color={theme.palette.secondary.main}>
+                                <Box display="flex" alignItems="center" gap="0.5rem">
+                                    <Typography variant="h4" fontWeight="bold">
+                                        ECOMVISION
+                                    </Typography>
+                                </Box>
+                                {!isNonMobile && (
+                                    <IconButton onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+                                        <ChevronLeft />
+                                    </IconButton>
+                                )}
+                            </FlexBetween>
+                        </Box>
+                        <List>
+                            {navItems.map(({ text, icon }) => {
+                                if (!icon) {
+                                    return (
+                                        <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                                            {text}
+                                        </Typography>
+                                    );
+                                }
+                                const lcText = text.toLowerCase();
 
-            </Drawer>
-            )]
-        </Box >
+                                return (
+                                    <ListItem key={text} disablePadding>
+                                        <ListItemButton
+                                            onClick={() => {
+                                                navigate(`/${lcText}`);
+                                                setActive(lcText);
+                                            }}
+                                            sx={{
+                                                backgroundColor:
+                                                    active === lcText
+                                                        ? theme.palette.secondary[300]
+                                                        : "transparent",
+                                                color:
+                                                    active === lcText
+                                                        ? theme.palette.primary[600]
+                                                        : theme.palette.secondary[100],
+                                            }}
+                                        >
+                                            <ListItemIcon
+                                                sx={{
+                                                    ml: "2rem",
+                                                    color:
+                                                        active === lcText
+                                                            ? theme.palette.primary[600]
+                                                            : theme.palette.secondary[200],
+                                                }}
+                                            >
+                                                {icon}
+                                            </ListItemIcon>
+                                            <ListItemText primary={text} />
+                                            {active === lcText && (
+                                                <ChevronRightOutlined sx={{ ml: "auto" }} />
+                                            )}
+                                        </ListItemButton>
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
+                    </Box>
+                </Drawer>
+            )}
+        </Box>
+
     )
 }
+
 
 export default Sidebar;
