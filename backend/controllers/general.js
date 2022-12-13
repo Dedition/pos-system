def9513e0ffe7f4cbb1760777cd4184dacc631cb
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-12 22:58:15 -0500

    Created new endpoint

--- backend/controllers/general.js
+++ backend/controllers/general.js
@@ -3,20 +3,9 @@ import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
     try {
-        const { id } = req.params;
-        const user = await User.findById(id);
+        const user = await User.findById(req.params.id);
         res.status(200).json(user);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-};
-
-
-// export const getUser = async (req, res) => {
-//     try {
-//         const user = await User.findById(req.params.id);
-//         res.status(200).json(user);
-//     } catch (error) {
-//         res.status(404).json({ message: error.message });
-//     }
-// }
+}
--- frontend/src/state/api.js
+++ frontend/src/state/api.js
@@ -3,14 +3,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: 'adminsApi',
-    tagTypes: ['User'],
+    tagTypes: ['User', 'Products'],
     endpoints: (builder) => ({
         getUser: builder.query({
             query: (id) => `general/user/${id}`,
             providesTags: ['User']
         }),
+        getProducts: builder.query({
+            query: () => `client/products`,
+            providesTags: ['Products']
+        }),
     }),
 });
 
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
