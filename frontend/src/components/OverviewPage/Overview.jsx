6e2f389040989dd6f5773c9888750b701278139d
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-13 18:31:19 -0500

    Created Overview jsx

--- frontend/src/components/OverviewPage/Overview.jsx
+++ frontend/src/components/OverviewPage/Overview.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import { Box, FormControl, MenuItem, Box, Select } from '@mui/material';
 
+import Header from 'components/HeaderPage/Header';
+import OverviewChart from './OverviewChart';
 
 const Overview = () => {
-    return (
-
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Overview" subtitle="Overview of revenue + profit" />
+      <Box height="75vh">
+        <FormControl sx={{ mt: "1rem" }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            label="View"
+            value={view}
+            onChange={(e) => setView(e.target.value)}
+          >
+            <MenuItem value="sales">Sales</MenuItem>
+            <MenuItem value="units">Units</MenuItem>
+          </Select>
+        </FormControl>
+        <OverviewChart view={view} />
+      </Box>
+    </Box>
   )
 };
 
