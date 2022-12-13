391170ac10be0581629f738087f7e436907580c5
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-13 16:31:36 -0500

    Added new route and package for CountryISO2-3

--- backend/controllers/client.js
+++ backend/controllers/client.js
@@ -65,3 +65,28 @@ export const getTransactions = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 };
+
+export const getGeography = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        const mappedLocations = users.reduce((acc, { country }) => {
+            const countryISO3 = getCountryIso3(country);
+            if (!acc[countryISO3]) {
+                acc[countryISO3] = 0;
+            }
+            acc[countryISO3]++;
+            return acc;
+        }, {});
+
+        const formattedLocations = Object.entries(mappedLocations).map(
+            ([country, count]) => {
+                return { id: country, value: count };
+            }
+        );
+
+        res.status(200).json(formattedLocations);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
