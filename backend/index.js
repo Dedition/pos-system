bc014ea73548218714cc9f4c4e4ef6ead94e0bff
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-13 18:17:17 -0500

    Inserted data for overallstats in Compass

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
+import OverallStat from './models/OverallStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
 
 /* CONFIG */
 dotenv.config();
@@ -50,4 +51,5 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
         // Product.insertMany(dataProduct);
         // ProductStat.insertMany(dataProductStat);
         // Transaction.insertMany(dataTransaction);
+        // OverallStat.insertMany(dataOverallStat);
     }).catch((err) => console.log(`${err} Did not connect`));
