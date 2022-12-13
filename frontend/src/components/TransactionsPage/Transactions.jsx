a594e17bb82687f18bfc1afef761682dfa4a4945
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-13 14:02:54 -0500

    Set up transactions data grid. Now creating custom toolbar

--- frontend/src/components/TransactionsPage/Transactions.jsx
+++ frontend/src/components/TransactionsPage/Transactions.jsx
@@ -36,8 +36,52 @@ const Transactions = () => {
     return (
         <Box m="1.5rem 2.5rem">
             <Header title="TRANSACTIONS" subtitle="Complete List of Transactions" />
-            <Box height="80vh">
-                <DataGrid />
+            <Box height="80vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid
+                    loading={isLoading || !data}
+                    getRowId={(row) => row._id}
+                    rows={(data && data.transactions) || []}
+                    columns={columns}
+                    rowCount={(data && data.total) || 0}
+                    rowsPerPageOptions={[20, 50, 100]}
+                    pagination
+                    page={page}
+                    pageSize={pageSize}
+                    paginationMode="server"
+                    sortingMode="server"
+                    onPageChange={(newPage) => setPage(newPage)}
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+                // components={{ Toolbar: DataGridCustomToolbar }}
+                // componentsProps={{
+                //     toolbar: { searchInput, setSearchInput, setSearch },
+                // }}
+                />
             </Box>
         </Box >
     )
