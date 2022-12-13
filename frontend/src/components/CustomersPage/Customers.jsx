e59d49452bafcd650a6e6555236369630ef2a6d5
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-12-13 12:30:17 -0500

    Styled customers appropriately

--- frontend/src/components/CustomersPage/Customers.jsx
+++ frontend/src/components/CustomersPage/Customers.jsx
@@ -16,7 +16,30 @@ const Customers = () => {
       <Box
         mt="40px"
         height="75vh"
-
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none"
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`
+          }
+        }}
       >
         <DataGrid
           loading={isLoading || !data}
@@ -25,7 +48,7 @@ const Customers = () => {
           columns={columns}
         />
       </Box>
-    </Box>
+    </Box >
   )
 }
 
